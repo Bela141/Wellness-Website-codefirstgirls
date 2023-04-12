98acb47d1daea482974bf404be21030ad776676c
Bela141 <126033761+Bela141@users.noreply.github.com>
2023-04-12 14:23:52 -0400

    Added new file

---
+++ main.js
@@ -0,0 +1,6 @@
+function navigateToBotton(){
+    window.scrollTo(0, document.body.scrollHeight);
+}
+function onFormSubmit(){
+    alert("Thank you for your request! Someone from our team will reach out soon.")
+}
